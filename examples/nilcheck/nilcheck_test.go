fdfa71bb82e51f6bf0dfd7a318f6931bdf4db960
Dave Cheney <dave@cheney.net>
2019-07-17 13:21:49 +1000

    sydney tweaks

--- examples/nilcheck/nilcheck_test.go
+++ examples/nilcheck/nilcheck_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 type T struct {
-	padding [8192]byte
+	padding [1 << 20]byte
 	buffer  [256]byte
 }
 
@@ -13,13 +13,26 @@ var t = new(T)
 
 func BenchmarkNilCheckNotElided(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		Result = t.buffer[10]
+		Result = t.buffer[1]
+		Result = t.buffer[2]
+		Result = t.buffer[4]
+		Result = t.buffer[8]
+		Result = t.buffer[16]
+		Result = t.buffer[32]
+		Result = t.buffer[64]
 	}
 }
 
 func BenchmarkNilCheckElided(b *testing.B) {
 	_ = t.buffer
 	for n := 0; n < b.N; n++ {
-		Result = t.buffer[10]
+		Result = t.buffer[1]
+		Result = t.buffer[2]
+		Result = t.buffer[4]
+		Result = t.buffer[8]
+		Result = t.buffer[16]
+		Result = t.buffer[32]
+		Result = t.buffer[64]
+
 	}
 }
